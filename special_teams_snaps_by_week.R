6fb3ba5ef95bf4970740095ae44810118540f573
Will Lankenau <will661944@gmail.com>
2025-08-19 23:24:08 -0400

    Weekly data for 2023and 2024. Draft data for 2025

--- special_teams_snaps_by_week.R
+++ special_teams_snaps_by_week.R
@@ -1,6 +1,6 @@
 
 
-d_past_week_st_snaps <- read_csv(eval(paste("~/R Stuff/FantasyFootball 2.0/weeklyStats/", This_Year, "/Special_Teams_Snaps.csv", sep = ""))) %>% 
+d_past_week_st_snaps <- read_csv(eval(paste("~/R Stuff/FantasyFootball 2.0/weeklyStats/", 2024, "/Special_Teams_Snaps.csv", sep = ""))) %>% 
   clean_names()
 
 a <- 1
@@ -8,7 +8,7 @@ while(a < 19){
   temp <- d_past_week_st_snaps %>% 
     filter(week == a)
   
-  write_csv(temp, eval(paste("~/R Stuff/FantasyFootball 2.0/weeklyStats/", This_Year, "/byWeek/Week_", a, "_ST_Snaps.csv", sep = "")))
+  write_csv(temp, eval(paste("~/R Stuff/FantasyFootball 2.0/weeklyStats/", 2024, "/byWeek/Week_", a, "_ST_Snaps.csv", sep = "")))
 
   a <- a+1
 }
