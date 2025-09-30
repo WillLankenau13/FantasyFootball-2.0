3380f105226a87fce8bc9b60f4a5f76cca1e74c7
Will Lankenau <will661944@gmail.com>
2025-09-30 00:18:33 -0500

    Edited starting qb determiner

--- starting_QB_determiner.R
+++ starting_QB_determiner.R
@@ -15,28 +15,34 @@ which_week <- 18
 
 #Years
 Years_Dataframe <- read_csv("~/R Stuff/FantasyFootball 2.0/Years_Dataframe.csv")
-Past_Year <- Years_Dataframe$Past_Year[1]
-This_Year <- Years_Dataframe$This_Year[1]
+This_Year <- 2022
 
 #files
-passing_stats <- read_csv(eval(paste("~/R Stuff/FantasyFootball 2.0/weeklyStats/", This_Year, "/weekly_passing_stats.csv", sep = ""))) %>%
-  clean_names()
-fpros_qb <- read_csv(eval(paste("~/R Stuff/FantasyFootball/Online_Models/Week_", which_week, "/FantasyPros_2022_Week_", which_week, "_QB_Rankings.csv", sep = ""))) %>% 
+fpros_qb <- read_csv(eval(paste("~/R Stuff/FantasyFootball 2.0/fantasyPros/", This_Year, "/FantasyPros_", This_Year, "_Week_", which_week, "_QB_Rankings.csv", sep = ""))) %>% 
   clean_names %>% 
   rename("player" = "player_name")
 
 #
 data <- player_names_func(fpros_qb)
 
+second_top_per_team <- data %>%
+  group_by(team) %>%
+  slice_max(order_by = proj_fpts, n = 2, with_ties = FALSE) %>%  # get top 2
+  arrange(team, desc(proj_fpts)) %>%  # highest first
+  slice(2) %>%  # take the second row = second-highest
+  select(team, player, proj_fpts) %>%
+  ungroup()
 
-# data <- data %>% 
-#   filter(player != "Jeff Driskel") %>% 
-#   filter(player != "Taylor Heinicke")
+max(second_top_per_team$proj_fpts)
+
+
+# data <- data %>%
+#   filter(player != "Anthony Richardson")
 
 ###
-data <- slice_max(data, proj_fpts, by = team)
+top <- slice_max(data, proj_fpts, by = team)
 
-starting_qbs <- data %>% 
+starting_qbs <- top %>% 
   select(team, player)
 
 colnames(starting_qbs) <- c("team", "QB")
@@ -44,7 +50,7 @@ colnames(starting_qbs) <- c("team", "QB")
 write_csv(starting_qbs, eval(paste("~/R Stuff/FantasyFootball 2.0/startingQBs/", This_Year, "/Week_", which_week, "_Starting_QBs.csv", sep = "")))
 
 
-
+max(second_top_per_team$proj_fpts)
 
 
 
